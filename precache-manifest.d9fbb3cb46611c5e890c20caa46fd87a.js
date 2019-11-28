eb0b8652e77220c8edfa37f6a8f9afdc95b40839
Alexandra Santos <alexandraps17@gmail.com>
2019-11-28 18:47:54 +0000

    Updates

---
+++ precache-manifest.d9fbb3cb46611c5e890c20caa46fd87a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fa26e17470190b018dc2e2850abbf0",
+    "url": "/alexandrapatriciosantos/GingerBreadMan/index.html"
+  },
+  {
+    "revision": "f6a70f7ebe77d70c8829",
+    "url": "/alexandrapatriciosantos/GingerBreadMan/static/css/main.f4024e78.chunk.css"
+  },
+  {
+    "revision": "ea92e522bd0e65078a18",
+    "url": "/alexandrapatriciosantos/GingerBreadMan/static/js/2.106dc600.chunk.js"
+  },
+  {
+    "revision": "f6a70f7ebe77d70c8829",
+    "url": "/alexandrapatriciosantos/GingerBreadMan/static/js/main.1ba79c22.chunk.js"
+  },
+  {
+    "revision": "bd2201da9e90ddbf062c",
+    "url": "/alexandrapatriciosantos/GingerBreadMan/static/js/runtime-main.3f6f174b.js"
+  }
+]);
